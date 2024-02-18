75a0abcb0be5a5c76e7f4356d69a460975bf6eb7
Vyacheslav Bespalov <vbespalov@ptsecurity.com>
2024-02-18 18:10:19 +0300

    Implement 'urlfetch' task

--- urlfetch/main.go
+++ urlfetch/main.go
@@ -2,6 +2,40 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	urls := os.Args[1:]
+
+	if len(urls) == 0 {
+		return
+	}
+
+	for _, url := range urls {
+		func() {
+			resp, err := http.Get(url)
+			check(err)
+
+			defer func(Body io.ReadCloser) {
+				err := Body.Close()
+				check(err)
+			}(resp.Body)
+
+			body, err := io.ReadAll(resp.Body)
+			check(err)
+
+			fmt.Println(string(body))
+		}()
+	}
 }
