b71dbab3494e1fd7df9d14cd81c5fdca7557b003
Vyacheslav Bespalov <vbespalov@ptsecurity.com>
2024-02-18 17:56:54 +0300

    Implement 'wordcount' task

--- wordcount/main.go
+++ wordcount/main.go
@@ -2,6 +2,46 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	files := os.Args[1:]
+
+	if len(files) == 0 {
+		return
+	}
+
+	result := make(map[string]int64)
+
+	for _, file := range files {
+		data, err := os.ReadFile(file)
+		check(err)
+
+		lines := strings.Split(string(data), "\n")
+
+		for _, line := range lines {
+			line = strings.TrimSpace(line)
+
+			elem, _ := result[line]
+			result[line] = elem + 1
+		}
+	}
+
+	for key, value := range result {
+		if value == 1 {
+			continue
+		}
+
+		fmt.Printf("%d\t%s\n", value, key)
+	}
 }
