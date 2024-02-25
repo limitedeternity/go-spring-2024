b8fb74cd205f40e7289011fcdac58ac325483e79
Vyacheslav Bespalov <vbespalov@ptsecurity.com>
2024-02-25 21:57:13 +0300

    Implement 'hogwarts' task

--- hogwarts/courselist.go
+++ hogwarts/courselist.go
@@ -3,5 +3,35 @@
 package hogwarts
 
 func GetCourseList(prereqs map[string][]string) []string {
-	return []string{}
+	learned := make(map[string]bool)
+	entered := make(map[string]bool)
+	result := make([]string, 0)
+
+	for course := range prereqs {
+		var impl func(string)
+		impl = func(course string) {
+			if entered[course] {
+				panic("Already entered")
+			}
+
+			if learned[course] {
+				return
+			}
+
+			entered[course] = true
+
+			for _, prereq := range prereqs[course] {
+				impl(prereq)
+			}
+
+			learned[course] = true
+			entered[course] = false
+
+			result = append(result, course)
+		}
+
+		impl(course)
+	}
+
+	return result
 }
