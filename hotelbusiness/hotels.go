36d365ca3bf01bfb77439e20528e68a39f17ed62
Vyacheslav Bespalov <vbespalov@ptsecurity.com>
2024-02-25 21:44:24 +0300

    Implement 'hotelbusiness' task

--- hotelbusiness/hotels.go
+++ hotelbusiness/hotels.go
@@ -13,5 +13,32 @@ type Load struct {
 }
 
 func ComputeLoad(guests []Guest) []Load {
-	return []Load{}
+	if len(guests) == 0 {
+		return []Load{}
+	}
+
+	maxOut := guests[0].CheckOutDate
+	for _, g := range guests {
+		maxOut = max(maxOut, g.CheckOutDate)
+	}
+
+	table := make([]int, maxOut+1)
+	for _, g := range guests {
+		table[g.CheckInDate] += 1
+		table[g.CheckOutDate] -= 1
+	}
+
+	result := make([]Load, 0)
+	count := 0
+
+	for date, delta := range table {
+		if delta == 0 {
+			continue
+		}
+
+		count += delta
+		result = append(result, Load{StartDate: date, GuestCount: count})
+	}
+
+	return result
 }
