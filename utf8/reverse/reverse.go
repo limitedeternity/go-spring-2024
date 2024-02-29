daa83640b3a676135fe82b7aa5c9cb44b6f3a8ae
Vyacheslav Bespalov <vbespalov@ptsecurity.com>
2024-02-29 19:37:56 +0300

    Implement 'utf8/reverse' task

--- utf8/reverse/reverse.go
+++ utf8/reverse/reverse.go
@@ -2,6 +2,21 @@
 
 package reverse
 
+import (
+	"strings"
+	"unicode/utf8"
+)
+
 func Reverse(input string) string {
-	return ""
+	var builder strings.Builder
+	builder.Grow(len(input))
+
+	cursor := 0
+	for i := utf8.RuneCountInString(input); i > 0; i-- {
+		r, size := utf8.DecodeLastRuneInString(input[:len(input)-cursor])
+		builder.WriteRune(r)
+		cursor += size
+	}
+
+	return builder.String()
 }
