236c0c9f6b44e9727ff262c038dff750451a28d5
Vyacheslav Bespalov <vbespalov@ptsecurity.com>
2024-02-29 20:22:59 +0300

    Implement 'utf8/spacecollapse' task

--- utf8/spacecollapse/collapse.go
+++ utf8/spacecollapse/collapse.go
@@ -2,6 +2,32 @@
 
 package spacecollapse
 
+import (
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
+
 func CollapseSpaces(input string) string {
-	return ""
+	var builder strings.Builder
+	builder.Grow(len(input))
+
+	cursor := 0
+	prevSpace := false
+
+	for i := utf8.RuneCountInString(input); i > 0; i-- {
+		r, size := utf8.DecodeRuneInString(input[cursor:])
+		cursor += size
+
+		isSpace := unicode.IsSpace(r)
+		if !isSpace {
+			builder.WriteRune(r)
+		} else if !prevSpace {
+			builder.WriteRune(' ')
+		}
+
+		prevSpace = isSpace
+	}
+
+	return builder.String()
 }
