a43332ba8305a7076c9a3a4a929ee1c86c0ddd64
Vyacheslav Bespalov <vbespalov@ptsecurity.com>
2024-03-11 23:46:24 +0300

    Implement 'yamlembed' task

--- yamlembed/types.go
+++ yamlembed/types.go
@@ -1,19 +1,50 @@
 package yamlembed
 
+import (
+	"strings"
+)
+
 type Foo struct {
-	A string
-	p int64
+	A string `yaml:"aa"`
+	p int64  `yaml:"-"`
 }
 
 type Bar struct {
-	I      int64
-	B      string
-	UpperB string
-	OI     []string
-	F      []any
+	I      int64    `yaml:"-"`
+	B      string   `yaml:"b"`
+	UpperB string   `yaml:"-"`
+	OI     []string `yaml:"oi,omitempty"`
+	F      []any    `yaml:"f,flow"`
+}
+
+func (b *Bar) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	type plain Bar
+	if err := unmarshal((*plain)(b)); err != nil {
+		return err
+	}
+
+	b.UpperB = strings.ToUpper(b.B)
+	return nil
 }
 
 type Baz struct {
 	Foo `yaml:",inline"`
 	Bar `yaml:",inline"`
+}
+
+func (b *Baz) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var foo Foo
+	var bar Bar
+
+	if err := unmarshal(&foo); err != nil {
+		return err
+	}
+
+	if err := unmarshal(&bar); err != nil {
+		return err
+	}
+
+	b.Foo = foo
+	b.Bar = bar
+	return nil
 }
