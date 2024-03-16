292610eae051ded3c91c8182e9a5e519beefc6ed
Vyacheslav Bespalov <vbespalov@ptsecurity.com>
2024-03-16 22:06:52 +0300

    Implement 'once' task

--- once/once.go
+++ once/once.go
@@ -4,16 +4,20 @@ package once
 
 // Once describes an object that will perform exactly one action.
 type Once struct {
+	once chan bool
+	run  chan bool
 }
 
 // New creates Once.
 func New() *Once {
-	return nil
+	return &Once{make(chan bool, 1), make(chan bool, 1)}
 }
 
 // Do calls the function f if and only if Do is being called for the
 // first time for this instance of Once. In other words, given
-// 	once := New()
+//
+//	once := New()
+//
 // if once.Do(f) is called multiple times, only the first call will invoke f,
 // even if f has a different value in each invocation. A new instance of
 // Once is required for each function to execute.
@@ -25,7 +29,13 @@ func New() *Once {
 //
 // If f panics, Do considers it to have returned; future calls of Do return
 // without calling f.
-//
 func (o *Once) Do(f func()) {
+	select {
+	case o.once <- true:
+		defer func() { o.run <- true }()
+		f()
 
+	case <-o.run:
+		defer func() { o.run <- false }()
+	}
 }
