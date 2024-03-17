aabb74f49274006628047442ad89f5713928029c
Vyacheslav Bespalov <vbespalov@ptsecurity.com>
2024-03-17 19:43:04 +0300

    Implement 'ratelimit' task

--- ratelimit/ratelimit.go
+++ ratelimit/ratelimit.go
@@ -10,6 +10,10 @@ import (
 
 // Limiter is precise rate limiter with context support.
 type Limiter struct {
+	maxCount int
+	interval time.Duration
+	timeouts []*time.Timer
+	stop     chan struct{}
 }
 
 var ErrStopped = errors.New("limiter stopped")
@@ -17,13 +21,47 @@ var ErrStopped = errors.New("limiter stopped")
 // NewLimiter returns limiter that throttles rate of successful Acquire() calls
 // to maxSize events at any given interval.
 func NewLimiter(maxCount int, interval time.Duration) *Limiter {
-	panic("not implemented")
+	timeouts := make([]*time.Timer, maxCount)
+
+	for i := range timeouts {
+		timeouts[i] = time.NewTimer(0)
+	}
+
+	return &Limiter{
+		maxCount: maxCount,
+		interval: interval,
+		timeouts: timeouts,
+		stop:     make(chan struct{}, 1),
+	}
 }
 
 func (l *Limiter) Acquire(ctx context.Context) error {
-	panic("not implemented")
+	select {
+	case <-l.stop:
+		return ErrStopped
+	default:
+	}
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	default:
+	}
+
+	for i := 0; ; i = (i + 1) % l.maxCount {
+		select {
+		case <-l.stop:
+			return ErrStopped
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-l.timeouts[i].C:
+			l.timeouts[i] = time.NewTimer(l.interval)
+			return nil
+		default:
+		}
+	}
 }
 
 func (l *Limiter) Stop() {
-	panic("not implemented")
+	close(l.stop)
 }
