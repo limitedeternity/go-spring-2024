cbf0dcb71a28a664b90700c6365483a3aba88f42
Vyacheslav Bespalov <vbespalov@ptsecurity.com>
2024-03-22 21:59:08 +0300

    Implement 'tparallel' task

--- tparallel/tparallel.go
+++ tparallel/tparallel.go
@@ -3,16 +3,67 @@
 package tparallel
 
 type T struct {
+	finished chan struct{}
+	barrier  chan struct{}
+	parallel bool
+	parent   *T
+	children []*T
+}
+
+func newT(parent *T) *T {
+	return &T{
+		finished: make(chan struct{}),
+		barrier:  make(chan struct{}),
+		parallel: false,
+		parent:   parent,
+		children: make([]*T, 0),
+	}
 }
 
 func (t *T) Parallel() {
-	panic("implement me")
+	if t.parallel {
+		panic("Already parallel")
+	}
+
+	t.parallel = true
+	t.parent.children = append(t.parent.children, t)
+
+	t.finished <- struct{}{}
+	<-t.parent.barrier
+}
+
+func (t *T) runner(subtest func(t *T)) {
+	subtest(t)
+	if len(t.children) > 0 {
+		close(t.barrier)
+
+		for _, child := range t.children {
+			<-child.finished
+		}
+	}
+
+	if t.parallel {
+		t.parent.finished <- struct{}{}
+	}
+
+	t.finished <- struct{}{}
 }
 
 func (t *T) Run(subtest func(t *T)) {
-	panic("implement me")
+	child := newT(t)
+	go child.runner(subtest)
+	<-child.finished
 }
 
 func Run(topTests []func(t *T)) {
-	panic("implement me")
+	root := newT(nil)
+	for _, subtest := range topTests {
+		root.Run(subtest)
+	}
+
+	close(root.barrier)
+
+	if len(root.children) > 0 {
+		<-root.finished
+	}
 }
