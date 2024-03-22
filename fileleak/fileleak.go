0ce108d5fe27f11d2b30a829393f7a965f6f62c9
Vyacheslav Bespalov <vbespalov@ptsecurity.com>
2024-03-22 21:02:42 +0300

    Implement 'fileleak' task

--- fileleak/fileleak.go
+++ fileleak/fileleak.go
@@ -2,11 +2,44 @@
 
 package fileleak
 
+import (
+	"io/fs"
+	"os"
+	"reflect"
+)
+
 type testingT interface {
 	Errorf(msg string, args ...interface{})
 	Cleanup(func())
 }
 
+func getOpenFiles() []fs.FileInfo {
+	entries, err := os.ReadDir("/proc/self/fd")
+	if err != nil {
+		panic(err)
+	}
+
+	infos := make([]fs.FileInfo, 0, len(entries))
+	for _, entry := range entries {
+		var info fs.FileInfo
+		info, err = entry.Info()
+
+		if err != nil {
+			panic(err)
+		}
+
+		infos = append(infos, info)
+	}
+
+	return infos
+}
+
 func VerifyNone(t testingT) {
-	panic("implement me")
+	before := getOpenFiles()
+	t.Cleanup(func() {
+		after := getOpenFiles()
+		if !reflect.DeepEqual(after, before) {
+			t.Errorf("Leaks detected. Before: %v, After: %v", before, after)
+		}
+	})
 }
